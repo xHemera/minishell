21342f813c8b4bb16e85d98e09f819e63d5bcb2a
xHemera <spleeth.dev@gmail.com>
2025-05-05 17:02:22 +0200

    J'attend la libft

--- src/env/env.c
+++ src/env/env.c
@@ -1,21 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tobesnar <tobesnar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/05 16:12:47 by tobesnar          #+#    #+#             */
-/*   Updated: 2025/05/05 16:55:32 by tobesnar         ###   ########.fr       */
+/*   Created: 2025/05/05 16:53:33 by tobesnar          #+#    #+#             */
+/*   Updated: 2025/05/05 16:54:23 by tobesnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include "../libft/libft.h"
-
-#endif
+#include "../../include/minishell.h"
--- src/main.c
+++ src/main.c
@@ -1,26 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tobesnar <tobesnar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 16:07:39 by tobesnar          #+#    #+#             */
-/*   Updated: 2025/05/05 16:14:38 by tobesnar         ###   ########.fr       */
+/*   Updated: 2025/05/05 17:01:54 by tobesnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(void)
+void	prompt_loop(char **env)
 {
 	char	*line;
 
 	while ("The prophecy is true")
 	{
 		line = readline("minishell> ");
-		if (!line)
-			printf("exit\n");
+		if (!line) //CTRL+D Renvoie NULL
+			break ;
+		if (*line)
+			add_history(line);
+		//TODO
+		free(line);
 	}
+}
+
+int	main(void)
+{
+
 	return (0);
 }
