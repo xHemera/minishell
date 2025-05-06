ce04941cf680e0563a25732aa094512a3a153378
xHemera <spleeth.dev@gmail.com>
2025-05-06 16:58:26 +0200

    Relocalisatin des fonctions de env

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tobesnar <tobesnar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 16:12:47 by tobesnar          #+#    #+#             */
-/*   Updated: 2025/05/05 17:51:33 by tobesnar         ###   ########.fr       */
+/*   Updated: 2025/05/06 16:57:03 by tobesnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <stdio.h>
 # include <stdbool.h>
+# include <stdlib.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "libft/includes/libft.h"
@@ -30,5 +31,13 @@ typedef struct s_shell
 {
 	t_env	env;
 }			t_shell;
+
+// ##################### //
+//       Fonctions       //
+// ##################### //
+
+// env_utils.c
+int		get_total_len(char **env);
+char	*copy_env(char **env);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,59 +6,11 @@
 /*   By: tobesnar <tobesnar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 16:07:39 by tobesnar          #+#    #+#             */
-/*   Updated: 2025/05/05 18:14:16 by tobesnar         ###   ########.fr       */
+/*   Updated: 2025/05/06 16:57:09 by tobesnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-
-#include <stdlib.h>
-
-static int	get_total_len(char **env)
-{
-	int	i;
-	int	j;
-	int	len;
-
-	i = 0;
-	len = 0;
-	while (env[i])
-	{
-		j = 0;
-		while (env[i][j])
-		{
-			len++;
-			j++;
-		}
-		i++;
-	}
-	return (len);
-}
-
-char	*copy_env(char **env)
-{
-	int		i;
-	int		j;
-	int		k;
-	char	*result;
-
-	result = malloc(sizeof(char) * (get_total_len(env) + 1));
-	if (!result)
-		return (NULL);
-	i = 0;
-	k = 0;
-	while (env[i])
-	{
-		j = 0;
-		while (env[i][j])
-		{
-			result[k++] = env[i][j++];
-		}
-		i++;
-	}
-	result[k] = '\0';
-	return (result);
-}
 
 int	main(int argc, char **argv, char **env)
 {
