5f7fc30a266717dfdd9bab88551fed582e6964f9
xHemera <spleeth.dev@gmail.com>
2025-05-22 18:01:13 +0200

    hentai

--- src/builtins/export/export.c
+++ src/builtins/export/export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tobesnar <tobesnar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:59:11 by tlize             #+#    #+#             */
-/*   Updated: 2025/05/22 17:47:24 by tlize            ###   ########.fr       */
+/*   Updated: 2025/05/22 17:54:57 by tobesnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,23 @@
 
 int ft_export(t_cmd *cmd, t_env *envp)
 {
-    t_env *test1;
-    
-    if (cmd->arg_count == 1)
-    {
-        while (envp->next)
-        {
-            ft_printf(envp->key);
-            ft_printf("=");
-            ft_printf(envp->value);
-            ft_printf("\n");
-            envp = envp->next;
-        }
-    }
-    else
-    {
-        test1 = env_new("test", "test");
-        return (1);
-    }
-    return (0);
+	t_env	*test1;
+
+	if (cmd->arg_count == 1)
+	{
+		while (envp->next)
+		{
+			ft_printf(envp->key);
+			ft_printf("=");
+			ft_printf(envp->value);
+			ft_printf("\n");
+			envp = envp->next;
+		}
+	}
+	else
+	{
+		test1 = env_new("test", "test");
+		return (1);
+	}
+	return (0);
 }
