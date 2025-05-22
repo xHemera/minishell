59209132c6d2f8c5b75e944caadefa0046f5fa7c
xHemera <spleeth.dev@gmail.com>
2025-05-22 17:45:04 +0200

    C mieux comme ca

--- src/parsing/tokenizer.c
+++ src/parsing/tokenizer.c
@@ -6,77 +6,73 @@
 /*   By: tobesnar <tobesnar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:12:15 by tobesnar          #+#    #+#             */
-/*   Updated: 2025/05/13 16:17:20 by tobesnar         ###   ########.fr       */
+/*   Updated: 2025/05/22 16:35:55 by tobesnar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+static int	find_token_end(char *line, int start)
+{
+	int	i;
+
+	i = start;
+	while (line[i])
+	{
+		if (line[i] == '\'' || line[i] == '\"')
+		{
+			if (!handle_quotes(line, &i, line[i]))
+				break ;
+		}
+		else if (is_whitespace(line[i]))
+			break ;
+		else
+			i++;
+	}
+	return (i);
+}
+
 static char	*extract_token(char *line, int *i)
 {
 	int		start;
-	char	quote;
-	char	*token;
+	int		end;
+	char	*raw_token;
+	char	*processed_token;
 
 	while (is_whitespace(line[*i]))
 		(*i)++;
 	start = *i;
-	quote = 0;
-	while (line[*i])
-	{
-		if (!quote && (line[*i] == '\'' || line[*i] == '\"'))
-			quote = line[(*i)++];
-		else if (quote && line[*i] == quote)
-		{
-			(*i)++;
-			quote = 0;
-		}
-		else if (!quote && is_whitespace(line[*i]))
-			break ;
-		else
-			(*i)++;
-	}
-	token = ft_substr(line, start, *i - start);
-	return (token);
-}
-
-static t_token	*add_token_to_list(t_token *head, char *token)
-{
-	t_token	*curr;
-	t_token	*tmp;
-
-	curr = malloc(sizeof(t_token));
-	if (!curr)
+	end = find_token_end(line, start);
+	*i = end;
+	if (end == start)
 		return (NULL);
-	curr->value = token;
-	curr->next = NULL;
-	if (!head)
-		return (curr);
-	else
-	{
-		tmp = head;
-		while (tmp->next)
-			tmp = tmp->next;
-		tmp->next = curr;
-	}
-	return (head);
+	raw_token = ft_substr(line, start, end - start);
+	if (!raw_token)
+		return (NULL);
+	processed_token = process_quoted_content(raw_token);
+	free(raw_token);
+	return (processed_token);
 }
 
 t_token	*tokenize_input(char *input)
 {
 	t_token	*head;
-	char	*token;
+	char	*token_value;
 	int		i;
 
 	head = NULL;
 	i = 0;
 	while (input[i])
 	{
-		token = extract_token(input, &i);
-		if (!token)
-			break ;
-		token = ft_strtrim(token, "\"\'");
-		head = add_token_to_list(head, token);
+		token_value = extract_token(input, &i);
+		if (!token_value)
+			continue ;
+		head = add_token_to_list(head, token_value);
+		if (!head)
+		{
+			token_list_clear(&head);
+			return (NULL);
+		}
 	}
 	return (head);
 }
