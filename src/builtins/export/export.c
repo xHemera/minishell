66167ebf1c6e322216b3178e06993600f343b6eb
zitriss <zitrissonline@gmail.com>
2025-05-27 18:58:08 +0200

    inshallah ca marche de toute facon j'y vais
    magnifique

--- src/builtins/export/export.c
+++ src/builtins/export/export.c
@@ -6,13 +6,28 @@
 /*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:59:11 by tlize             #+#    #+#             */
-/*   Updated: 2025/05/22 18:22:06 by tlize            ###   ########.fr       */
+/*   Updated: 2025/05/27 18:57:40 by tlize            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 //suicide garantit avant la fin de la fonction
+
+int export_add(char **cmd, t_env *envp)
+{
+    t_env   *new;
+    char    **supersplit;
+    int     i;
+
+    while (cmd[i])
+    {
+        supersplit = ft_split(cmd[i], '=');
+        new = env_new(supersplit[0], supersplit[1]);
+		env_add_back(envp, new);
+    }
+    return (1);
+}
 
 int ft_export(t_cmd *cmd, t_env *envp, int argc)
 {
@@ -28,11 +43,15 @@ int ft_export(t_cmd *cmd, t_env *envp, int argc)
             ft_printf("\n");
             envp = envp->next;
         }
+        return (1);
     }
     else
     {
-        test1 = env_new("test", "test");
-        return (1);
+        while (envp->next)
+        {
+            envp = envp->next;
+        }
+        return (export_add(cmd->args, envp));
     }
     return (0);
 }
